c4de54644882b8ca3bc52994e1865c180ab76532
zenbunosekimera <zenbunosekimera@gmail.com>
2026-08-30 01:29:01 +0900

    #52　ライセンス・クレジット画面を追加関連テキストを追加

--- title.gd
+++ title.gd
@@ -30,3 +30,7 @@ func _on_start_button_pressed() -> void:
 
 func _on_quit_button_pressed() -> void:
 	get_tree().quit()
+
+
+func _on_license_and_credits_button_pressed() -> void:
+		get_tree().change_scene_to_file("res://license_and_credits.tscn")
