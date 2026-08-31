e6008657f857c7428b12e8844c16265f1d7ff43c
kinako-omiso <k.rimu.0523@gmail.com>
2026-08-31 11:05:05 +0900

    #78 ドア効果音を追加

--- demobreaker/breaker.gd
+++ demobreaker/breaker.gd
@@ -4,6 +4,8 @@ signal lights_out
 
 var brearker_light = false
 
+@onready var breaker_sound_player: AudioStreamPlayer = $BreakerSoundPlayer
+
 func _ready() -> void:
 	$Breaker/BreakerLightG.light_energy = 1.0
 
@@ -18,5 +20,6 @@ func _process(delta: float) -> void:
 
 func _on_interactive_breaker_breaker_off() -> void:
 	brearker_light = true
+	breaker_sound_player.play()
 	lights_out.emit()
 	get_tree().call_group(&"elevator_doors", &"on_power_outage")
